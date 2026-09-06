eb330f74743dcf058df1661bbb5145bf49bf336b
Owen_Splat <Owen32Smart@gmail.com>
2026-09-06 00:20:47 -0400

    hook into the actual player class instead of what it inherits. Some stuff were effecting the wondering cats

--- Scripts/hooks.lua
+++ Scripts/hooks.lua
@@ -8,33 +8,33 @@ local settings = {
 }
 
 
-RegisterHook("/Game/AnimX/_Common/CharBP_Base.CharBP_Base_C:ReceiveTick", function(self, DeltaSeconds)
-    local Character = self:get()
-    if not Character or not Character:IsValid() then return end
+RegisterHook("/Game/AnimX/Cats/Realistic/CharBP_Cat_R_Player.CharBP_Cat_R_Player_C:ReceiveTick", function(self, DeltaSeconds)
+    local Player = FindFirstOf("CharBP_Cat_R_Player_C")
+    if not Player or not Player:IsValid() then return end
 
     if settings.CameraLock then
-        Character['Camera Speed - Pitch'] = 0.0
-        Character['Camera Speed - Yaw'] = 0.0
+        Player['Camera Speed - Pitch'] = 0.0
+        Player['Camera Speed - Yaw'] = 0.0
     else
         if settings.ReversedCamera then
-            Character['Camera Speed - Pitch'] = -0.6
-            Character['Camera Speed - Yaw'] = -0.6
+            Player['Camera Speed - Pitch'] = -0.6
+            Player['Camera Speed - Yaw'] = -0.6
         else
-            Character['Camera Speed - Pitch'] = 0.6
-            Character['Camera Speed - Yaw'] = 0.6
+            Player['Camera Speed - Pitch'] = 0.6
+            Player['Camera Speed - Yaw'] = 0.6
         end
     end
 
     if settings.ForcedJumps then
-        if Character['Is OnGround'] then
-            Character:DoJump(false, 1.0)
-            Character['Jump in Place'] = false
+        if Player['Is OnGround'] then
+            Player:DoJump(false, 1.0)
+            Player['Jump in Place'] = false
         end
     end
 
     -- This property is updated every frame so we don't need to manually set it back
     if settings.UpsideDown then
-        local Cam = Character.Camera
+        local Cam = Player.Camera
         if Cam and Cam:IsValid() then
             Cam.RelativeRotation.Roll = 180.0
         end
@@ -42,11 +42,11 @@ RegisterHook("/Game/AnimX/_Common/CharBP_Base.CharBP_Base_C:ReceiveTick", functi
 
     -- Same thing
     if settings.Frozen then
-        local MovementComp = Character.CharacterMovement
+        local MovementComp = Player.CharacterMovement
         if MovementComp and MovementComp:IsValid() then
             -- Launch player to end states like walls runs, mantling, etc
             local v3 = {X=0.0, Y=0.0, Z=0.0}
-            Character:LaunchWithForce(v3, 9.0, v3, true)
+            Player:LaunchWithForce(v3, 9.0, v3, true)
             -- Freeze player vel, still falls slowly so we make upward vel the default 9.8
             -- idk if it's perfect but it seems to work well
             MovementComp.Velocity = {X=0.0, Y=0.0, Z=9.8}
@@ -54,7 +54,7 @@ RegisterHook("/Game/AnimX/_Common/CharBP_Base.CharBP_Base_C:ReceiveTick", functi
     end
 
     if settings.LowGravity then
-        local MovementComp = Character.CharacterMovement
+        local MovementComp = Player.CharacterMovement
         if MovementComp and MovementComp:IsValid() then
             MovementComp.GravityScale = 0.25
         end
--- Scripts/main.lua
+++ Scripts/main.lua
@@ -13,11 +13,14 @@ local LoopRunning = false
 
 local CurrentSessionID = 0
 
+local BeginPlayHook = nil
+local EndPlayHook = nil
+
 -- These are the basic funcs that always have a chance to appear
 local BaseEffs = {
     EffectManager.ToggleDash,
     EffectManager.ToggleDoubleJump,
-    EffectManager.SpawnDrum,
+    -- EffectManager.SpawnDrum,
     EffectManager.SpawnYarnBall,
     EffectManager.InvertColors,
 }
@@ -46,116 +49,123 @@ end
 
 
 -- We want to clear effects and reset data when the player returns to the title screen
-RegisterHook("/Game/AnimX/_Common/CharBP_Base.CharBP_Base_C:ReceiveEndPlay", function(self)
-    if not self then return end
-    ResetUI()
-end)
+local function EndHook()
+    if not EndPlayHook == nil then return end
 
-
--- Start our loop when the player has been created
-RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self, newPawn)
-    if not self then return end
-
-    local PCon = UEHelpers.GetPlayerController()
-    if PCon and PCon:IsValid() then
-        local PChar = PCon.Character
-        if PChar and PChar:IsValid() then
-            print(PChar:GetFullName(), "\n")
-        end
-    end
-
-    CurrentSessionID = CurrentSessionID + 1
-    local MySessionID = CurrentSessionID
-
-    -- If the player is at the vet, don't run our randomizer or effect loops
-    local IsAtVet = false
-    local Carrier = FindFirstOf("BP_CatCarrierFromVet_C")
-    if Carrier and Carrier:IsValid() then
-        IsAtVet = true
-        Carrier:K2_DestroyActor()
-    end
-
-    if IsAtVet then
-        UIManager.HideTimer()
-        UIManager.SetText("You've been a bad kitty...")
-    else
+    EndPlayHook = RegisterHook("/Game/AnimX/Cats/Realistic/CharBP_Cat_R_Player.CharBP_Cat_R_Player_C:ReceiveEndPlay", function(self, EndPlayReason)
+        if not self then return end
         ResetUI()
-        Randomizer.Start()
-        EffectManager:InitHooks()
-        UIManager.SetText("Pending effect...")
-        UIManager.timerRaw = TimerData.Seconds
-        UIManager.UpdateTimer()
-        LoopRunning = true
-    end
-
-    TimerLoop = LoopAsync(1000, function()
-        if not LoopRunning or MySessionID ~= CurrentSessionID then
-            return true
-        end
-        if IsAtVet then
-            return true
-        end
-        UIManager.UpdateTimer()
-        return false
     end)
+end
 
-    EffectLoop = LoopAsync(TimerData.Seconds * 1000, function()
-        if not LoopRunning or MySessionID ~= CurrentSessionID then
-            return true
-        end
-        if IsAtVet then
-            return true
-        end
 
-        -- We still need to validate that the player exists
-        -- Otherwise we may run into a nil reference when the player exits to the title screen
-        local PlayerController = UEHelpers.GetPlayerController()
-        if not PlayerController or not PlayerController:IsValid() then
-            return true
-        end
-        local Character = PlayerController.Character
-        if not Character or not Character:IsValid() then
-            return true
-        end
+local function StartHook()
+    if not BeginPlayHook == nil then return end
 
-        -- We want to cleanup old effects (text and spawned objs) before adding a new effect
-        EffectManager.Cleanup()
-        UIManager.timerRaw = TimerData.Seconds
+    -- Start our loop when the player has been created
+    BeginPlayHook = RegisterHook("/Game/AnimX/Cats/Realistic/CharBP_Cat_R_Player.CharBP_Cat_R_Player_C:ReceiveBeginPlay", function(self)
+        if not self then return end
 
-        -- Keep track of the number of loops to control how likely some effects are
-        LoopCount = LoopCount + 1
-
-        -- Create our total list of effect funcs to run
-        local validFuncs = {}
-        for i = 1, #BaseEffs do
-            validFuncs[#validFuncs+1] = BaseEffs[i]
-        end
-
-        -- Some temporary effects are nuisances
-        -- So temp effects are only included in the pool every other loop
-        if LoopCount % 2 == 0 then
-            for i = 1, #TempEffs do
-                validFuncs[#validFuncs+1] = TempEffs[i]
+        local PCon = UEHelpers.GetPlayerController()
+        if PCon and PCon:IsValid() then
+            local PChar = PCon.Character
+            if PChar and PChar:IsValid() then
+                print(PChar:GetFullName(), "\n")
             end
         end
 
-        -- Random launches SUCK so they are only included in the pool every 5 loops
-        if LoopCount % 5 == 0 then
-            validFuncs[#validFuncs+1] = EffectManager.LaunchRandomDirection
+        CurrentSessionID = CurrentSessionID + 1
+        local MySessionID = CurrentSessionID
+
+        -- If the player is at the vet, don't run our randomizer or effect loops
+        local IsAtVet = false
+        local Carrier = FindFirstOf("BP_CatCarrierFromVet_C")
+        if Carrier and Carrier:IsValid() then
+            IsAtVet = true
         end
 
-        -- Vet visit isnt that bad since you can just leave
-        -- If I decide to force the player to complete it, then I'll lower the odds
-        -- I need to create a check that the player has beds
-        if LoopCount % 5 == 0 then
-            validFuncs[#validFuncs+1] = EffectManager.VetVisit
+        if IsAtVet then
+            UIManager.HideTimer()
+            UIManager.SetText("You've been a bad kitty...")
+        else
+            ResetUI()
+            Randomizer.Start()
+            EffectManager:InitHooks()
+            UIManager.SetText("Pending effect...")
+            UIManager.timerRaw = TimerData.Seconds
+            UIManager.UpdateTimer()
+            LoopRunning = true
         end
 
-        local randomIndex = math.random(1, #validFuncs)
-        validFuncs[randomIndex](Character)
-        return false
+        TimerLoop = LoopAsync(1000, function()
+            if not LoopRunning or MySessionID ~= CurrentSessionID then
+                return true
+            end
+            if IsAtVet then
+                return true
+            end
+            UIManager.UpdateTimer()
+            return false
+        end)
+
+        EffectLoop = LoopAsync(TimerData.Seconds * 1000, function()
+            if not LoopRunning or MySessionID ~= CurrentSessionID then
+                return true
+            end
+            if IsAtVet then
+                return true
+            end
+
+            -- We still need to validate that the player exists
+            -- Otherwise we may run into a nil reference when the player exits to the title screen
+            local Player = FindFirstOf("CharBP_Cat_R_Player_C")
+
+            -- We want to cleanup old effects (text and spawned objs) before adding a new effect
+            EffectManager.Cleanup()
+            UIManager.timerRaw = TimerData.Seconds
+
+            -- Keep track of the number of loops to control how likely some effects are
+            LoopCount = LoopCount + 1
+
+            -- Create our total list of effect funcs to run
+            local validFuncs = {}
+            for i = 1, #BaseEffs do
+                validFuncs[#validFuncs+1] = BaseEffs[i]
+            end
+
+            -- Some temporary effects are nuisances
+            -- So temp effects are only included in the pool every other loop
+            if LoopCount % 2 == 0 then
+                for i = 1, #TempEffs do
+                    validFuncs[#validFuncs+1] = TempEffs[i]
+                end
+            end
+
+            -- Random launches SUCK so they are only included in the pool every 5 loops
+            if LoopCount % 5 == 0 then
+                validFuncs[#validFuncs+1] = EffectManager.LaunchRandomDirection
+            end
+
+            -- Vet visit isnt that bad since you can just leave
+            -- If I decide to force the player to complete it, then I'll lower the odds
+            -- I need to create a check that the player has beds
+            if LoopCount % 5 == 0 then
+                validFuncs[#validFuncs+1] = EffectManager.VetVisit
+            end
+
+            local randomIndex = math.random(1, #validFuncs)
+            validFuncs[randomIndex](Player)
+            return false
+        end)
     end)
+end
+
+
+RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(self, newPawn)
+    StartHook()
+    EndHook()
 end)
+
 
 -- Create the seed when the mod loads
 math.randomseed(os.time())
